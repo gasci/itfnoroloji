80c564a10764b72ebea423dc5f435e588f1ced49
Göktuğ Aşcı <g.asci@hotmail.com>
2019-03-21 17:11:43 +0300

    latest edit

--- platforms/browser/www/static/bookpage.js
+++ platforms/browser/www/static/bookpage.js
@@ -1,17 +1,8 @@
-$.get("/static/head.html", function (data) {
-    $('head').append(data);
-});
-$.get("/static/navbar.html", function (data) {
-    $(data).insertBefore("body");
-    $('#nav-mobile li a').css('text-decoration', 'none').css('color', 'white');
-    $('a').css('text-decoration', 'none');
-});
-
 $(document).ready(function () {
 
-    $('body').addClass('container');
 
-    $('.sidenav').sidenav();
+    $('img').css('max-width', '100%');
+    $('img').css('height', 'auto');
 
 
 });
